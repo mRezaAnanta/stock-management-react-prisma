b5d22403c22adc4edd88b7eba483e1866a336f6f
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-26 09:42:46 +0700

    feat(app.jsx): add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { ProductProvider } from './context/ProductContext.jsx'
+import Products from './pages/Products.jsx'
 import './App.css'
-import ProductTable from './ProductTable.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <ProductTable />
-    </div>)
+    <ProductProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Products />} />
+        </Routes>
+      </Router>
+    </ProductProvider>
+  )
 }
 
 export default App
